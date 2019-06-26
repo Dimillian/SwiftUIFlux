53830e5aecf89dd6df9ffd4ee99012939a021ad9
Thomas Ricouard <ricouard77@gmail.com>
2019-06-26 13:48:20 +0200

    Added unit test as example

--- Tests/SwiftUIFluxTests/SwiftUIFluxTests.swift
+++ Tests/SwiftUIFluxTests/SwiftUIFluxTests.swift
@@ -1,12 +1,34 @@
 import XCTest
 @testable import SwiftUIFlux
 
+struct TestState: FluxState {
+    var count = 0
+}
+
+struct IncrementAction: Action { }
+
+func testReducer(state: TestState, action: Action) -> TestState {
+    var state = state
+    switch action {
+    case _ as IncrementAction:
+        state.count += 1
+    default:
+        break
+    }
+    return state
+}
+
+@available(iOS 13.0, *)
 final class SwiftUIFluxTests: XCTestCase {
-    func testExample() {
-        
+    let store = Store<TestState>(reducer: testReducer, state: TestState())
+    
+    func testStore() {
+        XCTAssert(store.state.count == 0, "Initial state is not valid")
+        store.dispatch(action: IncrementAction())
+        XCTAssert(store.state.count == 1, "Reduced state increment is not valid")
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testExample", testStore),
     ]
 }
